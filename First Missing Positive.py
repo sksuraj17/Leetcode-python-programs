1addb5bd5bec4955bff6510edf66628a26bbb820
Suraj SK <64256552+sksuraj17@users.noreply.github.com>
2022-07-16 18:49:44 +0530

    Update and rename First-missing-positive.py to First Missing Positive.py

--- First Missing Positive.py
+++ First Missing Positive.py
@@ -1,3 +1,5 @@
+# Difficulty - HARD ---> Given an unsorted integer array nums, return the smallest missing positive integer. You must implement an algorithm that runs in O(n) time and uses constant extra space.
+
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
         ans = 1
